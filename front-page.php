1b9d87302ec66259c5126e308f4a54327007694f
PalatusTerrivel <palatusterrivel@users.noreply.github.com>
2026-09-03 16:47:28 -0300

    Restaurando fundo azul-ceu, navbar Bootstrap, alinhamento dos botoes SVG e escondendo cabecalho padrao do Hello Elementor

--- front-page.php
+++ front-page.php
@@ -7,99 +7,99 @@ get_header();
 ?>
 
 <div class="text-center my-4">
-	<h1 class="titulo-principal display-3">Rhaokar</h1>
-	<h2 class="h4 text-muted">É o cenário de campanha onde se passarão os jogos</h2> 
+	<h1 class="titulo-principal display-3 font-weight-bold">Rhaokar</h1>
+	<h2 class="h5 text-dark">É o cenário de campanha onde se passarão os jogos</h2> 
 </div>
 
 <div class="container d-flex justify-content-center my-4">
-	<div class="row">
-		<div class="col align-self-center">
-			<ul class="center p-0" style="list-style-type:none;">
-				<li class="mb-2">
+	<div class="row w-100 justify-content-center">
+		<div class="col-12 text-center">
+			<ul class="list-unstyled d-inline-block text-center p-0 m-0">
+				<li class="mb-3">
 					<a href="<?php echo esc_url( home_url( '/racas/' ) ); ?>">
 						<svg class="botao" style="width: 270px; height: 70px;">
 							<polygon points="262,40 225,64 75,64 40,40 75,20 225,20" stroke="grey" fill="black" stroke-width="5"/>
-							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="25" fill="white" font-family="'NewRocker', Times, serif">
+							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="25" fill="white">
 								As Raças
 							</text>
 						</svg>
 					</a>
 				</li>
-				<li class="mb-2">
+				<li class="mb-3">
 					<a href="<?php echo esc_url( home_url( '/deuses/' ) ); ?>">
 						<svg class="botao" style="width: 270px; height: 70px;">
 							<polygon points="262,40 225,64 75,64 40,40 75,20 225,20" stroke="grey" fill="black" stroke-width="5"/>
-							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="25" fill="white" font-family="'NewRocker', Times, serif">
+							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="25" fill="white">
 								Os Deuses
 							</text>
 						</svg>
 					</a>
 				</li>
-				<li class="mb-2">
+				<li class="mb-3">
 					<a href="<?php echo esc_url( home_url( '/reinos/' ) ); ?>">
 						<svg class="botao" style="width: 270px; height: 70px;">
 							<polygon points="262,40 225,64 75,64 40,40 75,20 225,20" stroke="grey" fill="black" stroke-width="5"/>
-							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="25" fill="white" font-family="'NewRocker', Times, serif">
+							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="25" fill="white">
 								Os Reinos
 							</text>
 						</svg>
 					</a>
 				</li>
-				<li class="mb-2">
+				<li class="mb-3">
 					<a href="<?php echo esc_url( home_url( '/diario/' ) ); ?>">
 						<svg class="botao" style="width: 270px; height: 70px;">
 							<polygon points="262,40 225,64 75,64 40,40 75,20 225,20" stroke="grey" fill="black" stroke-width="5"/>
-							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="18" fill="white" font-family="'NewRocker', Times, serif">
+							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="18" fill="white">
 								Diário da Campanha
 							</text>
 						</svg>
 					</a>
 				</li>
-				<li class="mb-2">
+				<li class="mb-3">
 					<a href="<?php echo esc_url( home_url( '/magia/' ) ); ?>">
 						<svg class="botao" style="width: 270px; height: 70px;">
 							<polygon points="262,40 225,64 75,64 40,40 75,20 225,20" stroke="grey" fill="black" stroke-width="5"/>
-							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="25" fill="white" font-family="'NewRocker', Times, serif">
+							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="25" fill="white">
 								A Magia
 							</text>
 						</svg>
 					</a>
 				</li>
-				<li class="mb-2">
+				<li class="mb-3">
 					<a href="<?php echo esc_url( home_url( '/alquimia/' ) ); ?>">
 						<svg class="botao" style="width: 270px; height: 70px;">
 							<polygon points="262,40 225,64 75,64 40,40 75,20 225,20" stroke="grey" fill="black" stroke-width="5"/>
-							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="25" fill="white" font-family="'NewRocker', Times, serif">
+							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="25" fill="white">
 								A Alquimia
 							</text>
 						</svg>
 					</a>
 				</li>
-				<li class="mb-2">
+				<li class="mb-3">
 					<a href="<?php echo esc_url( home_url( '/tecnologia/' ) ); ?>">
 						<svg class="botao" style="width: 270px; height: 70px;">
 							<polygon points="262,40 225,64 75,64 40,40 75,20 225,20" stroke="grey" fill="black" stroke-width="5"/>
-							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="25" fill="white" font-family="'NewRocker', Times, serif">
+							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="25" fill="white">
 								A Tecnologia
 							</text>
 						</svg>
 					</a>
 				</li>
-				<li class="mb-2">
+				<li class="mb-3">
 					<a href="<?php echo esc_url( home_url( '/regras/' ) ); ?>">
 						<svg class="botao" style="width: 270px; height: 70px;">
 							<polygon points="262,40 225,64 75,64 40,40 75,20 225,20" stroke="grey" fill="black" stroke-width="5"/>
-							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="18" fill="white" font-family="'NewRocker', Times, serif">
+							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="18" fill="white">
 								Regras Alternativas
 							</text>
 						</svg>
 					</a>
 				</li>
-				<li class="mb-2">
-					<a href="#" data-toggle="modal" data-target="#conteudoModal2">
+				<li class="mb-3">
+					<a href="#conteudoModal2" data-toggle="modal" data-target="#conteudoModal2">
 						<svg class="botao" style="width: 270px; height: 70px;">
 							<polygon points="262,40 225,64 75,64 40,40 75,20 225,20" stroke="grey" fill="black" stroke-width="5"/>
-							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="25" fill="white" font-family="'NewRocker', Times, serif">
+							<text x="150" y="45" dominant-baseline="middle" text-anchor="middle" font-size="25" fill="white">
 								Créditos
 							</text>
 						</svg>  
@@ -111,38 +111,28 @@ get_header();
 </div>
 
 <!-- Modal de Créditos -->
-<div id="conteudoModal2" class="modal fade">
-	<div class="modal-dialog modal-dialog-centered">
+<div id="conteudoModal2" class="modal fade" tabindex="-1" role="dialog">
+	<div class="modal-dialog modal-dialog-centered" role="document">
 		<div class="modal-content">
-			<div class="modal-header">
-				<h4 class="modal-title">Créditos do Site</h4>
-				<button class="close" data-dismiss="modal" aria-label="fechar">
+			<div class="modal-header bg-dark text-white">
+				<h5 class="modal-title font-weight-bold">Créditos do Site</h5>
+				<button type="button" class="close text-white" data-dismiss="modal" aria-label="Fechar">
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div>
 			<div class="modal-body">
-				<ul>
-					<li><b>Cenário:</b> Paulo "Palatus" Oliveira, criação do cenário</li>
-					<li><b>Cenário:</b> Meus Jogadores, que deram muitas ideias para aplicar no cenário</li>
-					<li><b>Artes:</b> Brenno Progetti, responsável por todos os ícones e sprites em pixel art</li>
-					<li><b>Apoio Técnico:</b> Douglas "Doug" dos Anjos, sem o qual o mapa não ia funcionar nunca e ajuda com toda a parte técnica.</li>
+				<ul class="list-group list-group-flush text-left">
+					<li class="list-group-item"><b>Cenário:</b> Paulo "Palatus" Oliveira, criação do cenário</li>
+					<li class="list-group-item"><b>Cenário:</b> Meus Jogadores, que deram muitas ideias para aplicar no cenário</li>
+					<li class="list-group-item"><b>Artes:</b> Brenno Progetti, responsável por todos os ícones e sprites em pixel art</li>
+					<li class="list-group-item"><b>Apoio Técnico:</b> Douglas "Doug" dos Anjos, ajuda com toda a parte técnica.</li>
 				</ul>
 			</div>
 			<div class="modal-footer">
-				<button class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
 			</div>
 		</div>
 	</div> 
-</div>
-
-<!-- Suporte a Conteúdo do Elementor / WordPress -->
-<div class="container my-4">
-	<?php
-	while ( have_posts() ) :
-		the_post();
-		the_content();
-	endwhile;
-	?>
 </div>
 
 <?php
